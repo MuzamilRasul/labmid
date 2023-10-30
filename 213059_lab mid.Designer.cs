b0d6bd1dc87abd689465d7affe4cfc09b65b4534
MuzamilRasul <120672094+MuzamilRasul@users.noreply.github.com>
2023-10-30 11:37:56 +0500

    Add files via upload

---
+++ 213059_lab mid.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace QNO1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 306);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 213059_lab mid.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QNO1
+{
+    private void CalculateTotalButton_Click(object sender, EventArgs e)
+    {
+        decimal totalAmount = 0;
+
+        totalAmount += CalculateGroupTotal(groupBoxStarter);
+        totalAmount += CalculateGroupTotal(groupBoxMainCourse);
+        totalAmount += CalculateGroupTotal(groupBoxSweetDish);
+
+        txtTotalAmount.Text = totalAmount.ToString("C");
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            private decimal CalculateGroupTotal(GroupBox groupBox)
+            {
+                decimal groupTotal = 0;
+
+                foreach (Control control in groupBox.Controls)
+                {
+                    if (control is CheckBox checkbox)
+                    {
+                        if (checkbox.Checked)
+                        {
+                            string itemName = checkbox.Text;
+                            decimal itemPrice = GetItemPrice(groupBox, itemName);
+                            decimal quantity = GetItemQuantity(groupBox, itemName);
+
+                            groupTotal += itemPrice * quantity;
+                        }
+                    }
+                }
+
+                return groupTotal;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in groupBox.Controls)
+            {
+                if (control is Label label && label.Text.Contains(itemName))
+                {
+                    string[] parts = label.Text.Split('$');
+                    if (parts.Length > 1 && decimal.TryParse(parts[1], out decimal price))
+                    {
+                        return price;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            private decimal GetItemQuantity(GroupBox groupBox, string itemName)
+            {
+                foreach (Control control in groupBox.Controls)
+                {
+                    if (control is TextBox textBox && textBox.Name.Contains(itemName))
+                    {
+                        if (decimal.TryParse(textBox.Text, out decimal quantity))
+                        {
+                            return quantity;
+                        }
+                    }
+                }
+
+                return 0;
+            }
+        }
+    }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+  
+
+    
+
+ 
